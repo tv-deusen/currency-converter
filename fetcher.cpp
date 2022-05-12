eaf4191e9f7094b4df02105851c64c76078c557d
Tom <tomvandeusen@protonmail.com>
2022-05-12 14:21:39 +0800

    change api service and add json parsing

--- fetcher.cpp
+++ fetcher.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <sstream>
 #include <stdio.h>
+#include <nlohmann/json.hpp>
 #include "debug.h"
 #include "symbols.h"
 #include "fetcher.h"
+
+using json = nlohmann::json;
 
 Fetcher::Fetcher(FetcherConfigMap ConfigMap)
 {
@@ -17,7 +20,7 @@ Fetcher::Fetcher(FetcherConfigMap ConfigMap)
 std::string ApiKeyHeader(std::string Key)
 {
     std::stringstream Buf;
-    Buf << "@{\'apikey\':\'";
+    Buf << "@{\'access_key\'=\'";
     Buf << Key;
     Buf << "\'}";
 
@@ -26,25 +29,48 @@ std::string ApiKeyHeader(std::string Key)
 
 void Fetcher::GetCurrencySymbols()
 {
-    struct data config;
-    config.trace_ascii = 1; /* enable ascii tracing */
-
     auto SymbolsEndpoint = Config.Url() + Symbols;
-    std::cout << SymbolsEndpoint << std::endl;
+
     CURL *Curl = curl_easy_init();
     if (Curl != nullptr)
     {
+        // https://api.exchangerate.host
         curl_easy_setopt(Curl, CURLOPT_URL, SymbolsEndpoint.c_str());
-        // curl_easy_setopt(Curl, CURLOPT_HTTPHEADER, ApiKeyHeader(Config.Key()).c_str());
+        
         curl_easy_setopt(Curl, CURLOPT_WRITEFUNCTION, Fetcher::WriteCallback);
         curl_easy_setopt(Curl, CURLOPT_WRITEDATA, &ReadBuffer);
-        curl_easy_setopt(Curl, CURLOPT_VERBOSE, 1L);
-        curl_easy_setopt(Curl, CURLOPT_DEBUGDATA, &config);
-        curl_easy_setopt(Curl, CURLOPT_DEBUGFUNCTION, my_trace);
+
+        // struct data config;
+        // config.trace_ascii = 1; /* enable ascii tracing */       
+        // curl_easy_setopt(Curl, CURLOPT_VERBOSE, 1L);
+        // curl_easy_setopt(Curl, CURLOPT_DEBUGDATA, &config);
+        // curl_easy_setopt(Curl, CURLOPT_DEBUGFUNCTION, my_trace);
+        
         curl_easy_perform(Curl);
         curl_easy_cleanup(Curl);
     }
-    std::cout << ReadBuffer << std::endl;
+
+    json::parser_callback_t JsonParseCallback = [](int depth, json::parse_event_t event, json & parsed)
+    {
+        if (event == json::parse_event_t::key && parsed != json("symbols"))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    };
+
+    // auto FullResponse = json::parse(ReadBuffer);
+    // std::cout << std::setw(4) << FullResponse << std::endl;
+
+    auto Response = json::parse(ReadBuffer, JsonParseCallback);
+    if (!Response.is_discarded())
+    {
+        std::cout << Response << std::endl;
+    }
+    
 }
 
 size_t Fetcher::WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
